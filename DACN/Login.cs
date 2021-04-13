c2d428c6c73e952a6749df775cc2ec6270c7fd8e
letannam1999 <namlee151199@gmail.com>
2021-04-13 08:13:01 +0700

    Project: CheckAttendance Management non-function

--- DACN/Login.cs
+++ DACN/Login.cs
@@ -20,8 +20,8 @@ namespace DACN
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            if(txtName.Text=="admin" && txtPass.Text=="123")
-            { 
+            if (txtName.Text == "admin" && txtPass.Text == "123")
+            {
                 this.Hide();
                 Main_Form main_Form = new Main_Form();
                 main_Form.Show();
@@ -31,10 +31,12 @@ namespace DACN
                 if (txtName.Text.Trim().Equals(""))
                 {
                     MessageBox.Show("Enter Your UserName to Login", "Empty UserName", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }else if (txtPass.Text.Trim().Equals(""))
+                }
+                else if (txtPass.Text.Trim().Equals(""))
                 {
                     MessageBox.Show("Enter Your Password to Login", "Empty Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }else
+                }
+                else
                 {
                     MessageBox.Show("UserName or Password doesn't exists", "Wrong Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
